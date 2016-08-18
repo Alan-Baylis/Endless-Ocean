cad1eafa074f64cc4bc77ad3de8c78a13ea0e2a5
LanceHarris <lanceah88@gmail.com>
2016-08-18 16:45:59 +1200

    Pressure plates and doors added
    There's some issues with the animations and placing the button where we
    want. I found a work around with this by parenting a new gameObject with
    the pressureplate gameobject. Otherwise the pressureplate would just
    return back to where the animation starts.
    
    We can talk about this in person in our next meeting. Will get onto
    death conditions and tinker with this some more later. :)

---
+++ Endless Ocean/Assets/Scripts/Puzzle Scripts/PressurePlate.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+    // Is pressure plate down?
+    public bool pressurePlateDown = false;
+    // Animation controller
+    Animator animator;
+    // Door reference
+    public GameObject Door;
+    // Door Animator
+    Animator doorAnimator;
+
+    // Use this for initialization
+    void Start () {
+        this.animator = this.GetComponent<Animator>();
+        this.doorAnimator = this.Door.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.animator.SetBool("PressurePlateDown", this.pressurePlateDown);
+        this.doorAnimator.SetBool("PressurePlateDown", this.pressurePlateDown);
+        if (pressurePlateDown)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Box" || col.gameObject.name == "Character")
+        {
+            pressurePlateDown = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider col)
+    {
+            pressurePlateDown = false;
+    }
+}
