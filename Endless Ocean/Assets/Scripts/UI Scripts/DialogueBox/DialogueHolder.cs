ad49821273dc78ee85a4529362a1512b5084bab9
LanceHarris <lanceah88@gmail.com>
2016-09-26 13:07:51 +1300

    Dialogue box added
    Had to put the "click to continue" button on R for now, since if it's on
    the same button that triggers the conversation, it'll just restart it
    quickly at the end. Will update this when I find a solution

---
+++ Endless Ocean/Assets/Scripts/UI Scripts/DialogueBox/DialogueHolder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueHolder : MonoBehaviour {
+
+    private DialogueManager dialogueManager;
+
+    public string[] dialogueLines;
+
+	// Use this for initialization
+	void Start () {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                if (!dialogueManager.dialogueActive)
+                {
+                    Time.timeScale = 0;
+                    dialogueManager.currentLine = 0;
+                    dialogueManager.currentLetter = 0;
+                    dialogueManager.dialogueLines = this.dialogueLines;
+                    dialogueManager.showDialogue();
+                }
+            }
+        }
+    }
+}
---
+++ Endless Ocean/Assets/Scripts/UI Scripts/DialogueBox/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dialogueBox;
+    public Text textBody;
+
+    public bool dialogueActive;
+
+    public string[] dialogueLines;
+    public int currentLine;
+
+    public float letterPause = 0.6f;
+    public int currentLetter = 0;
+    public AudioClip typeSound1;
+    public AudioClip typeSound2;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //next dialogue box
+        // On R temporarily until I can find a solution
+	    if(dialogueActive && Input.GetKeyDown(KeyCode.R))
+        {
+            currentLine++;
+            textBody.text = "";
+            currentLetter = 0;
+        }
+        // if all lines exhauseted, stop writing message
+        if(currentLine >= dialogueLines.Length)
+        {
+            dialogueBox.SetActive(false);
+            dialogueActive = false;
+            currentLine = 0;
+            textBody.text = "";
+            Time.timeScale = 1;
+        }
+
+        if (dialogueActive)
+        {
+            StartCoroutine(TypeText());
+        }
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in dialogueLines[currentLine])
+        {
+            if (currentLetter != dialogueLines[currentLine].Length)
+            {
+                textBody.text += dialogueLines[currentLine][currentLetter];
+                if (typeSound1 && typeSound2)
+                {
+                    //SoundManager.instance.RandomizeSfx(typeSound1, typeSound2);
+                    yield return 0;
+                }
+                currentLetter++;
+                yield return new WaitForSeconds(letterPause);
+            }
+        }
+    }
+
+    public void showDialogue()
+    {
+        textBody.text = "";
+        dialogueActive = true;
+        dialogueBox.SetActive(true);
+    }
+}
