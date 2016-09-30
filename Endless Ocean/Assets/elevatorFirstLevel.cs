b7384facf3b9621b7bd540497f83c79aba02ef62
LanceHarris <lanceah88@gmail.com>
2016-10-01 11:09:37 +1300

    Elevator added

---
+++ Endless Ocean/Assets/elevatorFirstLevel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class elevatorFirstLevel : MonoBehaviour
+{
+    bool triggered;
+    public Animator elevatorAC;
+
+    // Use this for initialization
+    void Start()
+    {
+        triggered = false;
+        this.elevatorAC = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Player" && !triggered)
+        {
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                triggered = true;
+                this.elevatorAC.SetBool("triggered", true);
+            }
+        }
+    }
+}
---
+++ Endless Ocean/Assets/interactablePromptToggle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class interactablePromptToggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
