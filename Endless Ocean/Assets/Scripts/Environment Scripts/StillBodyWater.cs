368563be414592adbde25178293bafa68ba67c66
Fman72 <Kearnsmain@gmail.com>
2016-10-01 12:31:29 +1300

    Added scene transition and improved water physics

--- Endless Ocean/Assets/Scripts/Environment Scripts/StillBodyWater.cs
+++ Endless Ocean/Assets/Scripts/Environment Scripts/StillBodyWater.cs
@@ -2,11 +2,18 @@
 using System.Collections;
 
 /// <summary>
-/// This is the class fo srill bodies of water. It handles the physics simulation for this water.
+/// This is the class for still bodies of water. It handles the physics simulation for this water.
+/// 
+/// The formula used for simulating floating comes from this thread https://forum.unity3d.com/threads/floating-a-object-on-water.31671/
 /// </summary>
 public class StillBodyWater : MonoBehaviour {
 
-    public int buoyancyForce;
+    private int floatHeight = 1;
+    //Int with the water level where water stops.
+    private float waterLevel = 0;
+    //An int to decresen the force lifting up objects that 'float'
+    private float bounceDamp = 0.5f;
+
 
     /// <summary>
     /// Runs whenever an object is inside the water. Adds a buoyancy fornce to the object to make it 'float'.
@@ -14,17 +21,22 @@ public class StillBodyWater : MonoBehaviour {
     /// <param name="col">The other collider in the water.</param>
     void OnTriggerStay(Collider col)
     {
-        col.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, this.buoyancyForce, 0));
-        Debug.Log("Contact");
+        if (col.GetComponent<Rigidbody>() != null)
+        {
+            float floatForceFactor = 1f - ((col.gameObject.transform.position.y - this.waterLevel) / floatHeight);
+            if(floatForceFactor > 0)
+            {
+                Vector3 floatForce = -Physics.gravity * (floatForceFactor - col.GetComponent<Rigidbody>().velocity.y * bounceDamp);
+                col.GetComponent<Rigidbody>().AddForce(floatForce);
+            }
+        }
     }
 
     /// <summary>
-    /// Runs whenever an object is inside the water. Adds a buoyancy fornce to the object to make it 'float'.
+    /// Initializes key variables.
     /// </summary>
-    /// <param name="col">The other collider in the water.</param>
-    void OnTriggerEnter(Collider col)
+    void Start()
     {
-        col.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, this.buoyancyForce, 0));
-        Debug.Log("Contact");
+        this.waterLevel = this.gameObject.transform.position.y;
     }
 }
---
+++ Endless Ocean/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script attatches to a collider so that when the player enters it they transition to a new scene.
+/// </summary>
+public class SceneTransition : MonoBehaviour {
+
+    public string sceneName;
+
+    /// <summary>
+    /// Loads the new scene when the player enters the trigger collider.
+    /// </summary>
+    /// <param name="col">The collider of the object that entered the trigger collider.</param>
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(this.sceneName);
+        }
+    }
+}
