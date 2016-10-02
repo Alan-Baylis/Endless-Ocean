63b9c1c8cf50dc21ccb1b7eadc2ede683876235e
Fman72 <Kearnsmain@gmail.com>
2016-10-02 15:25:10 +1300

    Adding water physics for floating objects.

---
+++ Endless Ocean/Assets/Scripts/Environment Scripts/RisingWaterLevel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This class should be applied to water. It makes the water rise.
+/// </summary>
+public class RisingWaterLevel : MonoBehaviour {
+
+    public float amountToRiseBy;
+    public float riseIncrement;
+
+    private float currentHeightDifference;
+	
+	/// <summary>
+    /// Raises the water level each frame. 
+    /// </summary>
+	void Update () {
+        if (currentHeightDifference < amountToRiseBy)
+        {
+            this.currentHeightDifference += riseIncrement;
+            Vector3 newPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + riseIncrement, this.gameObject.transform.position.z);
+            this.gameObject.transform.position = newPosition;
+        }
+        else
+        {
+            this.enabled = false;
+        }
+	}
+}
--- Endless Ocean/Assets/Scripts/Environment Scripts/StillBodyWater.cs
+++ Endless Ocean/Assets/Scripts/Environment Scripts/StillBodyWater.cs
@@ -6,13 +6,17 @@ using System.Collections;
 /// 
 /// The formula used for simulating floating comes from this thread https://forum.unity3d.com/threads/floating-a-object-on-water.31671/
 /// </summary>
-public class StillBodyWater : MonoBehaviour {
+public class StillBodyWater : MonoBehaviour
+{
 
     private int floatHeight = 1;
     //Int with the water level where water stops.
     private float waterLevel = 0;
     //An int to decresen the force lifting up objects that 'float'
-    private float bounceDamp = 0.5f;
+    private float bounceDamp = .4f;
+
+    private float moveSpeedRemoved;
+
 
 
     /// <summary>
@@ -21,14 +25,47 @@ public class StillBodyWater : MonoBehaviour {
     /// <param name="col">The other collider in the water.</param>
     void OnTriggerStay(Collider col)
     {
-        if (col.GetComponent<Rigidbody>() != null)
+        if (col.GetComponent<Rigidbody>() != null && !col.gameObject.CompareTag("Player"))
         {
             float floatForceFactor = 1f - ((col.gameObject.transform.position.y - this.waterLevel) / floatHeight);
-            if(floatForceFactor > 0)
+            if (floatForceFactor > 0)
             {
                 Vector3 floatForce = -Physics.gravity * (floatForceFactor - col.GetComponent<Rigidbody>().velocity.y * bounceDamp);
+                //Making the float force bigger if it is very small to make the objects more floaty.
+                //if (floatForce.y > 9 && floatForce.y < 10)
+                //{
+                //    floatForce.y--;
+                //}
+                Debug.Log(floatForce.y);
                 col.GetComponent<Rigidbody>().AddForce(floatForce);
             }
+        }
+    }
+
+    /// <summary>
+    /// Makes the player move slower and jump higher in water.
+    /// </summary>
+    /// <param name="other">The other collider.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            this.moveSpeedRemoved = (other.GetComponent<PlayerController>().movementSpeed);
+            other.gameObject.GetComponent<PlayerController>().movementSpeed /= 2;
+            other.gameObject.GetComponent<Rigidbody>().mass /= 2;
+        }
+    }
+
+    /// <summary>
+    /// Returns the players movement to normal when they leave the water.
+    /// </summary>
+    /// <param name="other">The other collider.</param>
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().movementSpeed += this.moveSpeedRemoved;
+            other.gameObject.GetComponent<Rigidbody>().mass *= 2;
         }
     }
 
@@ -36,6 +73,11 @@ public class StillBodyWater : MonoBehaviour {
     /// Initializes key variables.
     /// </summary>
     void Start()
+    {
+        this.waterLevel = this.gameObject.transform.position.y;
+    }
+
+    void FixedUpdate()
     {
         this.waterLevel = this.gameObject.transform.position.y;
     }
