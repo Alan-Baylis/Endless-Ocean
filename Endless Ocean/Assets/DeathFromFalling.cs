6b6830cf7f86eb7fbaf5ab2a250edc3d22c28822
Fman72 <Kearnsmain@gmail.com>
2016-10-04 15:57:11 +1300

    Fixing death collider on level 2

--- Endless Ocean/Assets/DeathFromFalling.cs
+++ Endless Ocean/Assets/DeathFromFalling.cs
@@ -5,9 +5,11 @@ public class DeathFromFalling : MonoBehaviour {
 
     void OnTriggerEnter(Collider col)
     {
-            if (col.gameObject.name == "Character")
-            {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Destroy(col.gameObject);
             Application.LoadLevel(Application.loadedLevel);
+            col.gameObject.transform.position = Vector3.zero;
         }
     }
 }
